ff1c016e2ddfd046a02eaa3cb852c7a046006477
rodrigo <rodrigorozendobastos@gmail.com>
2016-05-08 02:39:20 -0300

    First example

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by rodrigo on 07/05/16.
+ */
+'use strict';
+
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '234748140:AAHL-_IviLOhFw0yoxYR7Mhawn66lc7lGQU';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+// Matches /echo [whatever]
+bot.onText(/\/echo (.+)/, function (msg, match) {
+	var fromId = msg.from.id;
+	var resp = match[1];
+	bot.sendMessage(fromId, resp);
+});
+
+// Any kind of message
+bot.on('message', function (msg) {
+
+	var chatId = msg.chat.id;
+	// photo can be: a file path, a stream or a Telegram file_id
+	var photo = 'pug.jpg';
+	bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+});
+
